b643d07bc9483d494696749f74d55efdeb7f7ea7
Jaswant Panchumarti <jspanchu@gmail.com>
2021-10-10 10:58:03 +0200

    Create wasm-emscripten.sh

---
+++ wasm-emscripten.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+# Requires an emsdk installation. See https://github.com/emscripten-core/emsdk
+# This script expects the environment var CMAKE_TOOLCHAIN_FILE = ${EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
+# Usage:
+#  ./wasm-emscripten.sh <vtk_source_dir> <vtk_build_dir>
+# Example:
+#  ./wasm-emscripten.sh ~/projects/vtk ~/projects/vtk/build
+#
+
+VTK_SOURCE_DIR = $1
+VTK_BUILD_DIR = $2
+
+emcmake cmake \
+ -S $VTK_SOURCE_DIR \
+ -H $VTK_BUILD_DIR \
+ -G Ninja \
+ -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} \
+ -DBUILD_SHARED_LIBS=OFF \
+ -DCMAKE_BUILD_TYPE=Release \
+ -DVTK_ENABLE_LOGGING=OFF \
+ -DVTK_ENABLE_WRAPPING=OFF \
+ -DVTK_GROUP_ENABLE_Imaging=NO \
+ -DVTK_GROUP_ENABLE_MPI=NO \
+ -DVTK_GROUP_ENABLE_Qt=NO \
+ -DVTK_GROUP_ENABLE_Rendering=WANT \
+ -DVTK_GROUP_ENABLE_StandAlone=WANT \
+ -DVTK_GROUP_ENABLE_Views=NO  \
+ -DVTK_GROUP_ENABLE_Web=NO \
+ -DVTK_LEGACY_REMOVE=ON \
+ -DVTK_NO_PLATFORM_SOCKETS=ON \
+ -DVTK_MODULE_ENABLE_VTK_hdf5=NO \
+ -DVTK_MODULE_ENABLE_VTK_RenderingContextOpenGL2=DONT_WANT \
+ -DVTK_OPENGL_USE_GLES=ON
+ 
